e8e99279bca729af309771145122eb5c205dd180
James Mulvenna <jamespmulvenna@gmail.com>
2017-10-10 14:16:53 -0400

    Added deliverables
    for reference

--- app/src/main/java/com/example/yue/nexttext/UI/MessageConfigureActivity.kt
+++ app/src/main/java/com/example/yue/nexttext/UI/MessageConfigureActivity.kt
@@ -71,7 +71,7 @@ class MessageConfigureActivity : AppCompatActivity() {
     class SMSObjectFragment: Fragment(){
         override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
             val view = inflater?.inflate(R.layout.fragment_sms_object, container, false)
-            textView_sms_frag?.text = resources.getString(R.string.sms)
+
             return view
         }
     }
