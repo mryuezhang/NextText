466fa819e77e4a308fffc3778a730fafa8b52cdb
Devon Plouffe <devon.plouffe@carleton.ca>
2017-10-23 14:47:52 -0400

    alpha location services

--- app/src/main/java/com/example/yue/nexttext/Data/Location.java
+++
@@ -1,8 +0,0 @@
-package com.example.yue.nexttext.Data;
-
-/**
- * Created by jamesmulvenna on 2017-10-13.
- */
-
-public class Location {
-}
---
+++ app/src/main/java/com/example/yue/nexttext/Data/MLocation.java
@@ -0,0 +1,32 @@
+package com.example.yue.nexttext.Data;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.Locale;
+/**
+ * Created by devonplouffe on 2017-10-22.
+ */
+
+public class MLocation extends LocationRequest {
+
+
+}
