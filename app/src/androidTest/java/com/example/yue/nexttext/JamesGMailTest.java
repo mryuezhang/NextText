51d0a2cd6dd37ed810c9f8565b9df5731c165410
James Mulvenna <jamespmulvenna@gmail.com>
2017-11-23 14:21:02 -0500

    Revert "Oral exam stuff, Gmail confirmation"
    This reverts commit 55df6804d47532f048c81eb62ec0737ebaffbd9f.

--- app/src/androidTest/java/com/example/yue/nexttext/JamesGMailTest.java
+++ app/src/androidTest/java/com/example/yue/nexttext/JamesGMailTest.java
@@ -6,23 +6,20 @@ package com.example.yue.nexttext;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
-import android.util.Log;
 
 import com.example.yue.nexttext.Core.EmailService.GMailSender;
 import com.example.yue.nexttext.DataType.Message;
 import com.example.yue.nexttext.DataType.MessageWrapper;
-import com.example.yue.nexttext.UI.EmailSettings;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 
 public class JamesGMailTest {
 
-    /*@Test
+    @Test
     public void testGMail() {
         Context context = InstrumentationRegistry.getTargetContext();
-        Message message = new Message("jamespmulvenna@gmail.com", "admld", "jamespmulvenna@gmail.com", "test subject", "test content");
+        Message message = new Message("EMAIL FROM HERE", "PASSWORD HERE", "EMAIL TO HERE", "test subject", "test content");
         MessageWrapper messageWrapper = new MessageWrapper(message);
 
         GMailSender gMailSender = new GMailSender(message.get_from(), message.get_password());
@@ -32,16 +29,6 @@ public class JamesGMailTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
-    */
-    @Test
-    public void testGmailAuthenticate(){
-        EmailSettings newEmail = new EmailSettings("jamespmulvenna@gmail.com", "asklzmV!");
-
-        int actualCode;
-        newEmail.sendConfirmationEmail();
-        //boolean result = newEmail.validatedEmail(8734, actualCode);
-
     }
 
 }
--- app/src/main/java/com/example/yue/nexttext/UI/EmailSettings.java
+++ app/src/main/java/com/example/yue/nexttext/UI/EmailSettings.java
@@ -1,63 +1,36 @@
 package com.example.yue.nexttext.UI;
 
+/**
+ * Created by jamesmulvenna on 2017-11-19.
+ */
+
+
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
-import android.util.Log;
-
-import com.example.yue.nexttext.UI.Utilities;
+import android.widget.Toast;
 
 import com.example.yue.nexttext.Core.EmailService.GMailSender;
 import com.example.yue.nexttext.DataType.Message;
 
-import java.util.Random;
-
-/**
- * Created by jamesmulvenna on 2017-11-19.
- */
+import javax.mail.PasswordAuthentication;
 
 public class EmailSettings {
     private String username;
     private String password;
 
-    public EmailSettings(String user, String pass){
-        this.username = user;
-        this.password = pass;
+    public EmailSettings(){
     }
 
-    public String getUsername(){
-        return this.username;
-    }
-
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getPassword(){
-        return this.password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
-    }
-
-
-
-    public void sendConfirmationEmail() {
-        Log.d(null, "Got to sending the email");
-        Random r = new Random();
-        int code = r.nextInt(9999 - 1000) + 1000;
-        final Message message = new Message("jamespmulvenna@gmail.com", "asklzmV!", username, "Confirmation Code", Integer.toString(code));
-        Log.d(null, username);
+    public void authenticateEmail(final String newUsername, final String newPassword) {
         @SuppressLint("StaticFieldLeak") AsyncTask<String, Void, Integer> myAsync = new AsyncTask<String, Void, Integer>() {
 
             @Override
             protected Integer doInBackground(String... strings) {
-                GMailSender gMailSender = new GMailSender(message.get_from(), message.get_password());
+                GMailSender gMailSender = new GMailSender(newUsername, newPassword);
 
                 int checkError;
 
                 try {
-                    gMailSender.sendMail(message.get_subject(), message.get_content(), message.get_from(), message.get_to());
                     checkError = 0;
                 } catch (Exception ex) {
                     checkError = 1;
@@ -69,27 +42,19 @@ public class EmailSettings {
             @Override
             protected void onPostExecute(Integer result) {
                 super.onPostExecute(result);
-                if (result == 0) {
-                    //succeed, delete message from messagelist and database
-                    Log.d(null, "success");
-
-                } else {
-                    //failed, try again?
-                    Log.d(null, "fail");
-
-
+                if(result == 0){
+                    //succeed
+                    Toast.makeText(null, "You can now send Gmail messages.", Toast.LENGTH_LONG).show();
+                    username = newUsername;
+                    password = newPassword;
+                }
+                else{
+                    //failed
+                    Toast.makeText(null,"Account is invalid, please try again.",Toast.LENGTH_LONG).show();
                 }
             }
         };
-        myAsync.execute(message.get_from(), message.get_password(), message.get_subject(), message.get_content(), message.get_to());
-        Log.d(null, "Got to end");
-    }
-
-    public Boolean validatedEmail(int expectedCode, int actualCode){
-        if (expectedCode == actualCode){
-            return true;
-        } else {
-            return false;
-        }
+        myAsync.execute(newUsername,newPassword,"Sign up Message Sequence","",
+                newUsername);
     }
 }
